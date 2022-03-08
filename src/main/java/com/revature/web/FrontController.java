45c163477ed686895a4fffcd9276a09cc4b920ea
Simon Coble <simon291@revature.net>
2022-03-08 15:44:58 -0500

    add begining of FrontController project

---
+++ src/main/java/com/revature/web/FrontController.java
@@ -0,0 +1,36 @@
+package com.revature.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.revature.util.RequestHelper;
+
+/**
+ * Servlet implementation class FrontController
+ */
+public class FrontController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		final String URI = request.getRequestURI().replace("/FrontController/", "");
+		
+		switch(URI) {
+		case "login":
+			RequestHelper.processLogin(request, response);
+			break;
+		case "employees":
+			break;
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
